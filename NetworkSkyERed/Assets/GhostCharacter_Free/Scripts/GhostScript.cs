7a474c6bddc8d5fd10a23f8b01688405c24d784a
RadekKozaczuk <radekkozaczuk1@gmail.com>
2025-01-23 14:50:53 +0100

    code simplification

--- NetworkSkyERed/Assets/GhostCharacter_Free/Scripts/GhostScript.cs
+++ NetworkSkyERed/Assets/GhostCharacter_Free/Scripts/GhostScript.cs
@@ -1,191 +1,205 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Sample {
 public class GhostScript : MonoBehaviour
 {
-    private Animator Anim;
-    private CharacterController Ctrl;
-    private Vector3 MoveDirection = Vector3.zero;
+    Animator _anim;
+    CharacterController _ctrl;
+    Vector3 _moveDirection = Vector3.zero;
+
     // Cache hash values
-    private static readonly int IdleState = Animator.StringToHash("Base Layer.idle");
-    private static readonly int MoveState = Animator.StringToHash("Base Layer.move");
-    private static readonly int SurprisedState = Animator.StringToHash("Base Layer.surprised");
-    private static readonly int AttackState = Animator.StringToHash("Base Layer.attack_shift");
-    private static readonly int DissolveState = Animator.StringToHash("Base Layer.dissolve");
-    private static readonly int AttackTag = Animator.StringToHash("Attack");
+    static readonly int _idleState = Animator.StringToHash("Base Layer.idle");
+    static readonly int _moveState = Animator.StringToHash("Base Layer.move");
+    static readonly int _surprisedState = Animator.StringToHash("Base Layer.surprised");
+    static readonly int _attackState = Animator.StringToHash("Base Layer.attack_shift");
+    static readonly int _dissolveState = Animator.StringToHash("Base Layer.dissolve");
+    static readonly int _attackTag = Animator.StringToHash("Attack");
+    
     // dissolve
-    [SerializeField] private SkinnedMeshRenderer[] MeshR;
-    private float Dissolve_value = 1;
-    private bool DissolveFlg = false;
-    private const int maxHP = 3;
-    private int HP = maxHP;
-    private Text HP_text;
+    [SerializeField]
+    SkinnedMeshRenderer[] MeshR;
+    float _dissolveValue = 1;
+    bool _dissolveFlg;
+    const int MaxHp = 3;
+    int _hp = MaxHp;
+    Text _hpText;
 
     // moving speed
-    [SerializeField] private float Speed = 4;
+    [SerializeField]
+    float Speed = 4;
 
     void Start()
     {
-        Anim = this.GetComponent<Animator>();
-        Ctrl = this.GetComponent<CharacterController>();
-        HP_text = GameObject.Find("Canvas/HP").GetComponent<Text>();
-        HP_text.text = "HP " + HP.ToString();
+        _anim = GetComponent<Animator>();
+        _ctrl = GetComponent<CharacterController>();
+        _hpText = GameObject.Find("Canvas/HP").GetComponent<Text>();
+        _hpText.text = "HP " + _hp;
     }
 
     void Update()
     {
-        STATUS();
-        GRAVITY();
-        Respawn();
+        Status();
+        Gravity();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Respawn();
+
         // this character status
-        if(!PlayerStatus.ContainsValue( true ))
+        if(!_playerStatus.ContainsValue( true ))
         {
-            MOVE();
+            Move();
             PlayerAttack();
             Damage();
         }
-        else if(PlayerStatus.ContainsValue( true ))
+        else if(_playerStatus.ContainsValue( true ))
         {
-            int status_name = 0;
-            foreach(var i in PlayerStatus)
+            int statusName = 0;
+            foreach(KeyValuePair<int, bool> i in _playerStatus)
             {
-                if(i.Value == true)
+                if (i.Value)
                 {
-                    status_name = i.Key;
+                    statusName = i.Key;
                     break;
                 }
             }
-            if(status_name == Dissolve)
+
+            if(statusName == Dissolve)
             {
                 PlayerDissolve();
             }
-            else if(status_name == Attack)
+            else if(statusName == Attack)
             {
                 PlayerAttack();
             }
-            else if(status_name == Surprised)
+            else if(statusName == Surprised)
             {
                 // nothing method
             }
         }
+
         // Dissolve
-        if(HP <= 0 && !DissolveFlg)
+        if(_hp <= 0 && !_dissolveFlg)
         {
-            Anim.CrossFade(DissolveState, 0.1f, 0, 0);
-            DissolveFlg = true;
+            _anim.CrossFade(_dissolveState, 0.1f, 0, 0);
+            _dissolveFlg = true;
         }
         // processing at respawn
-        else if(HP == maxHP && DissolveFlg)
+        else if(_hp == MaxHp && _dissolveFlg)
         {
-            DissolveFlg = false;
+            _dissolveFlg = false;
         }
     }
 
     //---------------------------------------------------------------------
     // character status
     //---------------------------------------------------------------------
-    private const int Dissolve = 1;
-    private const int Attack = 2;
-    private const int Surprised = 3;
-    private Dictionary<int, bool> PlayerStatus = new Dictionary<int, bool>
+    const int Dissolve = 1;
+    const int Attack = 2;
+    const int Surprised = 3;
+    readonly Dictionary<int, bool> _playerStatus = new()
     {
         {Dissolve, false },
         {Attack, false },
         {Surprised, false },
     };
+    static readonly int _dissolve = Shader.PropertyToID("_Dissolve");
+
     //------------------------------
-    private void STATUS ()
+    void Status ()
     {
         // during dissolve
-        if(DissolveFlg && HP <= 0)
+        if(_dissolveFlg && _hp <= 0)
         {
-            PlayerStatus[Dissolve] = true;
+            _playerStatus[Dissolve] = true;
         }
-        else if(!DissolveFlg)
+        else if(!_dissolveFlg)
         {
-            PlayerStatus[Dissolve] = false;
+            _playerStatus[Dissolve] = false;
         }
+        
         // during attacking
-        if(Anim.GetCurrentAnimatorStateInfo(0).tagHash == AttackTag)
+        if(_anim.GetCurrentAnimatorStateInfo(0).tagHash == _attackTag)
         {
-            PlayerStatus[Attack] = true;
+            _playerStatus[Attack] = true;
         }
-        else if(Anim.GetCurrentAnimatorStateInfo(0).tagHash != AttackTag)
+        else if(_anim.GetCurrentAnimatorStateInfo(0).tagHash != _attackTag)
         {
-            PlayerStatus[Attack] = false;
+            _playerStatus[Attack] = false;
         }
+        
         // during damaging
-        if(Anim.GetCurrentAnimatorStateInfo(0).fullPathHash == SurprisedState)
+        if(_anim.GetCurrentAnimatorStateInfo(0).fullPathHash == _surprisedState)
         {
-            PlayerStatus[Surprised] = true;
+            _playerStatus[Surprised] = true;
         }
-        else if(Anim.GetCurrentAnimatorStateInfo(0).fullPathHash != SurprisedState)
+        else if(_anim.GetCurrentAnimatorStateInfo(0).fullPathHash != _surprisedState)
         {
-            PlayerStatus[Surprised] = false;
+            _playerStatus[Surprised] = false;
         }
     }
     // dissolve shading
-    private void PlayerDissolve ()
+    void PlayerDissolve ()
     {
-        Dissolve_value -= Time.deltaTime;
-        for(int i = 0; i < MeshR.Length; i++)
+        _dissolveValue -= Time.deltaTime;
+        foreach (SkinnedMeshRenderer mesh in MeshR)
+            mesh.material.SetFloat(_dissolve, _dissolveValue);
+
+        if(_dissolveValue <= 0)
         {
-            MeshR[i].material.SetFloat("_Dissolve", Dissolve_value);
-        }
-        if(Dissolve_value <= 0)
-        {
-            Ctrl.enabled = false;
+            _ctrl.enabled = false;
         }
     }
+    
     // play a animation of Attack
-    private void PlayerAttack ()
+    void PlayerAttack ()
     {
         if(Input.GetKeyDown(KeyCode.A))
         {
-            Anim.CrossFade(AttackState,0.1f,0,0);
+            _anim.CrossFade(_attackState,0.1f,0,0);
         }
     }
+    
     //---------------------------------------------------------------------
     // gravity for fall of this character
     //---------------------------------------------------------------------
-    private void GRAVITY ()
+    void Gravity ()
     {
-        if(Ctrl.enabled)
+        if(_ctrl.enabled)
         {
             if(CheckGrounded())
             {
-                if(MoveDirection.y < -0.1f)
+                if(_moveDirection.y < -0.1f)
                 {
-                    MoveDirection.y = -0.1f;
+                    _moveDirection.y = -0.1f;
                 }
             }
-            MoveDirection.y -= 0.1f;
-            Ctrl.Move(MoveDirection * Time.deltaTime);
+            _moveDirection.y -= 0.1f;
+            _ctrl.Move(_moveDirection * Time.deltaTime);
         }
     }
+    
     //---------------------------------------------------------------------
     // whether it is grounded
     //---------------------------------------------------------------------
-    private bool CheckGrounded()
+    bool CheckGrounded()
     {
-        if (Ctrl.isGrounded && Ctrl.enabled)
+        if (_ctrl.isGrounded && _ctrl.enabled)
         {
             return true;
         }
-        Ray ray = new Ray(this.transform.position + Vector3.up * 0.1f, Vector3.down);
+        Ray ray = new Ray(transform.position + Vector3.up * 0.1f, Vector3.down);
         float range = 0.2f;
         return Physics.Raycast(ray, range);
     }
+    
     //---------------------------------------------------------------------
     // for slime moving
     //---------------------------------------------------------------------
-    private void MOVE ()
+    void Move ()
     {
         // velocity
-        if(Anim.GetCurrentAnimatorStateInfo(0).fullPathHash == MoveState)
+        if(_anim.GetCurrentAnimatorStateInfo(0).fullPathHash == _moveState)
         {
             if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
             {
@@ -204,116 +218,115 @@ public class GhostScript : MonoBehaviour
                 MOVE_Velocity(new Vector3(-Speed, 0, 0), new Vector3(0, 270, 0));
             }
         }
-        KEY_DOWN();
-        KEY_UP();
+        KeyDown();
+        KeyUp();
     }
+
     //---------------------------------------------------------------------
     // value for moving
     //---------------------------------------------------------------------
-    private void MOVE_Velocity (Vector3 velocity, Vector3 rot)
+    void MOVE_Velocity (Vector3 velocity, Vector3 rot)
     {
-        MoveDirection = new Vector3 (velocity.x, MoveDirection.y, velocity.z);
-        if(Ctrl.enabled)
+        _moveDirection = new Vector3 (velocity.x, _moveDirection.y, velocity.z);
+        if(_ctrl.enabled)
         {
-            Ctrl.Move(MoveDirection * Time.deltaTime);
+            _ctrl.Move(_moveDirection * Time.deltaTime);
         }
-        MoveDirection.x = 0;
-        MoveDirection.z = 0;
-        this.transform.rotation = Quaternion.Euler(rot);
+        _moveDirection.x = 0;
+        _moveDirection.z = 0;
+        transform.rotation = Quaternion.Euler(rot);
     }
     //---------------------------------------------------------------------
     // whether arrow key is key down
     //---------------------------------------------------------------------
-    private void KEY_DOWN ()
+    void KeyDown ()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Anim.CrossFade(MoveState, 0.1f, 0, 0);
+            _anim.CrossFade(_moveState, 0.1f, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Anim.CrossFade(MoveState, 0.1f, 0, 0);
+            _anim.CrossFade(_moveState, 0.1f, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            Anim.CrossFade(MoveState, 0.1f, 0, 0);
+            _anim.CrossFade(_moveState, 0.1f, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Anim.CrossFade(MoveState, 0.1f, 0, 0);
+            _anim.CrossFade(_moveState, 0.1f, 0, 0);
         }
     }
+    
     //---------------------------------------------------------------------
     // whether arrow key is key up
     //---------------------------------------------------------------------
-    private void KEY_UP ()
+    void KeyUp ()
     {
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
             if(!Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
             {
-                Anim.CrossFade(IdleState, 0.1f, 0, 0);
+                _anim.CrossFade(_idleState, 0.1f, 0, 0);
             }
         }
         else if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             if(!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
             {
-                Anim.CrossFade(IdleState, 0.1f, 0, 0);
+                _anim.CrossFade(_idleState, 0.1f, 0, 0);
             }
         }
         else if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             if(!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.RightArrow))
             {
-                Anim.CrossFade(IdleState, 0.1f, 0, 0);
+                _anim.CrossFade(_idleState, 0.1f, 0, 0);
             }
         }
         else if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             if(!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.LeftArrow))
             {
-                Anim.CrossFade(IdleState, 0.1f, 0, 0);
+                _anim.CrossFade(_idleState, 0.1f, 0, 0);
             }
         }
     }
+    
     //---------------------------------------------------------------------
     // damage
     //---------------------------------------------------------------------
-    private void Damage ()
+    void Damage ()
     {
         // Damaged by outside field.
         if(Input.GetKeyUp(KeyCode.S))
         {
-            Anim.CrossFade(SurprisedState, 0.1f, 0, 0);
-            HP--;
-            HP_text.text = "HP " + HP.ToString();
+            _anim.CrossFade(_surprisedState, 0.1f, 0, 0);
+            _hp--;
+            _hpText.text = "HP " + _hp.ToString();
         }
     }
+    
     //---------------------------------------------------------------------
     // respawn
     //---------------------------------------------------------------------
-    private void Respawn ()
+    void Respawn ()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            // player HP
-            HP = maxHP;
-            
-            Ctrl.enabled = false;
-            this.transform.position = Vector3.zero; // player position
-            this.transform.rotation = Quaternion.Euler(Vector3.zero); // player facing
-            Ctrl.enabled = true;
-            
-            // reset Dissolve
-            Dissolve_value = 1;
-            for(int i = 0; i < MeshR.Length; i++)
-            {
-                MeshR[i].material.SetFloat("_Dissolve", Dissolve_value);
-            }
-            // reset animation
-            Anim.CrossFade(IdleState, 0.1f, 0, 0);
-        }
+        // player HP
+        _hp = MaxHp;
+        
+        _ctrl.enabled = false;
+        transform.position = Vector3.zero; // player position
+        transform.rotation = Quaternion.Euler(Vector3.zero); // player facing
+        _ctrl.enabled = true;
+        
+        // reset Dissolve
+        _dissolveValue = 1;
+        foreach (SkinnedMeshRenderer mesh in MeshR)
+            mesh.material.SetFloat(_dissolve, _dissolveValue);
+
+        // reset animation
+        _anim.CrossFade(_idleState, 0.1f, 0, 0);
     }
-}
 }
